a105854378bbe30a61459df532eaca046d8736c3
Alex Shch <xantax@ukr.net>
2019-08-08 21:45:13 +0300

    add text/plain support

--- codec.go
+++ codec.go
@@ -3,14 +3,22 @@ package codec
 import "io"
 
 // Encoder is responsible for data encoding.
-type Encoder interface {
-	Encode(v interface{}) error
-}
+type Encoder interface{ Encode(v interface{}) error }
+
+// EncoderFunc is a wrapper that allows using regular function as Encoder interface.
+type EncoderFunc func(interface{}) error
+
+// Encode encodes provided interface to the writer.
+func (fn EncoderFunc) Encode(src interface{}) error { return fn(src) }
 
 // Decoder is responsible for data decoding.
-type Decoder interface {
-	Decode(v interface{}) error
-}
+type Decoder interface{ Decode(v interface{}) error }
+
+// DecoderFunc is a wrapper that allows using regular function as Decoder interface.
+type DecoderFunc func(interface{}) error
+
+// Decode decodes data from the reader to provided receiver.
+func (fn DecoderFunc) Decode(recv interface{}) error { return fn(recv) }
 
 // Codec can create Encoder(s) and Decoder(s) with provided io.Reader/io.Writer.
 type Codec interface {
---
+++ driver/text/text.go
@@ -0,0 +1,67 @@
+package json
+
+import (
+	"bytes"
+	"encoding"
+	"fmt"
+	"io"
+
+	"github.com/tiny-go/codec"
+	"github.com/tiny-go/codec/driver"
+)
+
+// DataTypeText contains plain text codec data type.
+const DataTypeText = "text/plain"
+
+var global = &Text{}
+
+// Text plain codec.
+type Text struct{}
+
+// Encoder creates text encoder.
+func (t *Text) Encoder(w io.Writer) codec.Encoder {
+	return codec.EncoderFunc(func(src interface{}) error {
+		if marshaler, ok := src.(encoding.TextMarshaler); ok {
+			data, err := marshaler.MarshalText()
+			if err != nil {
+				return err
+			}
+			_, err = w.Write(data)
+			return err
+		}
+		fmt.Fprintf(w, "%s", src)
+		return nil
+	})
+}
+
+// Decoder creates text decoder.
+func (t *Text) Decoder(r io.Reader) codec.Decoder {
+	return codec.DecoderFunc(func(recv interface{}) error {
+		var buf = new(bytes.Buffer)
+		if _, err := buf.ReadFrom(r); err != nil {
+			return err
+		}
+		if unmarshaler, ok := recv.(encoding.TextUnmarshaler); ok {
+			return unmarshaler.UnmarshalText(buf.Bytes())
+		}
+		switch t := recv.(type) {
+		case *string:
+			*t = buf.String()
+			return nil
+		case *[]byte:
+			*t = buf.Bytes()
+			return nil
+		default:
+			return fmt.Errorf("cannot unmarshal into variable of type %T", t)
+		}
+	})
+}
+
+// MimeType returns the mime type of the codec.
+func (t *Text) MimeType() string { return DataTypeText }
+
+func init() {
+	// since text is a stateless codec we can always return the same instance in order
+	// to save some memory
+	driver.Register(DataTypeText, func(string) codec.Codec { return global })
+}
